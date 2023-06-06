86bd7500e3a69ed19a54c870727ba00b6745e0cd
Narongyot <kgvkgv20@gmail.com>
2023-06-06 15:42:09 +0700

    FEAT : add get-reward smart contract service

---
+++ src/pages/services/contract/get-reward.tsx
@@ -0,0 +1,20 @@
+import {
+  useContractRead,
+} from "wagmi";
+
+import type { Address } from "wagmi";
+import { ethers } from "ethers";
+import { lottofactoryabi } from "../../../../abi/lotto-factory";
+
+const GetReward = (installment: string) => {
+  const { data, isLoading, isError, isSuccess } = useContractRead({
+    address: process.env.NEXT_PUBLIC_LOTTO_FACTORY as Address,
+    abi: lottofactoryabi,
+    functionName: "getReward",
+    args: [ethers.BigNumber.from(installment)],
+  });
+
+  return { data: data!, isError, isLoading };
+};
+
+export default GetReward;
