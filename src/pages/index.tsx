e77283a99722884e94f39045b19d84f8a588392c
Narongyot <kgvkgv20@gmail.com>
2023-06-07 12:21:33 +0700

    FEAT : Add pictures index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import FlipCard, { BackCard, FrontCard } from "@/components/Flipcard/Flipcard";
 import { motion } from "framer-motion";
-import NFT from "public/images/nft.png";
+import NFT from "public/images/Lottery.png";
 import Navbar from "@/components/Navbar/navbar-before-login";
 import { useAccount } from "wagmi";
 import React, { useEffect, useState } from "react";
 import GetInstallment from "./services/contract/get-installment";
 
-
 export default function Home() {
   const { address } = useAccount();
   const [lotteryNumber, setLotteryNumber] = useState("");
   const { data, isError, isLoading } = GetInstallment();
-  
+
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
@@ -29,9 +28,11 @@ export default function Home() {
 
   return (
     mounted && (
-      <>
-        HOME
-      </>
+      <div className="flex justify-center items-center h-screen">
+        <div className="place-item-center">
+          <Image src={NFT} width="500" height="500" alt="Thai Lottery NFT" />
+        </div>
+      </div>
     )
   );
 }
