6afc95976e1cc3128cfe80018c17e27a1c9be4d7
Narongyot <kgvkgv20@gmail.com>
2023-06-07 12:20:44 +0700

    FIX : fix get reward smart contract service

--- src/pages/services/contract/get-reward.tsx
+++ src/pages/services/contract/get-reward.tsx
@@ -1,6 +1,4 @@
-import {
-  useContractRead,
-} from "wagmi";
+import { useContractRead } from "wagmi";
 
 import type { Address } from "wagmi";
 import { ethers } from "ethers";
@@ -8,12 +6,17 @@ import { lottofactoryabi } from "../../../../abi/lotto-factory";
 
 const GetReward = (installment: string) => {
   const { data, isLoading, isError, isSuccess } = useContractRead({
-    address: process.env.NEXT_PUBLIC_LOTTO_FACTORY as Address,
+    // address: process.env.NEXT_PUBLIC_LOTTO_FACTORY as Address,
+    address: "0x799087Cb7e416667AD9712bAFC07eb7b6Bb71E2C" as Address,
     abi: lottofactoryabi,
     functionName: "getReward",
     args: [ethers.BigNumber.from(installment)],
+    watch: true,
   });
   console.log("Reward data:", data);
+  console.log("isLoading,:", isLoading);
+  console.log("isError:", isError);
+  console.log("isSuccess:", isSuccess);
   return { data: data!, isError, isLoading };
 };
 
